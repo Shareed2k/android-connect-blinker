981954ee5a1f155bd23bc24b26019de5729a44e3
Jason A. Donenfeld <Jason@zx2c4.com>
2012-04-02 05:55:00 +0200

    Print ip address.

--- inet-connect-hook.c
+++ inet-connect-hook.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/net.h>
-#include <linux/mm.h>
+#include <linux/socket.h>
+#include <linux/in.h>
 
 extern struct proto_ops inet_stream_ops;
 
@@ -23,11 +24,17 @@ static void enable_page_protection(void)
 	}
 }
 
-int (*old_connect)(struct socket *sock, struct sockaddr *vaddr, int sockaddr_len, int flags);
-int connect(struct socket *sock, struct sockaddr *vaddr, int sockaddr_len, int flags)
+int (*old_connect)(struct socket *sock, struct sockaddr *uaddr, int sockaddr_len, int flags);
+int connect(struct socket *sock, struct sockaddr *uaddr, int sockaddr_len, int flags)
 {
-	printk(KERN_ALERT "Hooked connect.");
-	return (*old_connect)(sock, vaddr, sockaddr_len, flags);
+	struct sockaddr_in *usin = (struct sockaddr_in *)uaddr;
+	unsigned char bytes[4];
+	bytes[0] = (usin->sin_addr.s_addr >> 0) & 0xFF;
+	bytes[1] = (usin->sin_addr.s_addr >> 8) & 0xFF;
+	bytes[2] = (usin->sin_addr.s_addr >> 16) & 0xFF;
+	bytes[3] = (usin->sin_addr.s_addr >> 24) & 0xFF;
+	printk(KERN_ALERT "Hooked connect to: %d.%d.%d.%d", bytes[0], bytes[1], bytes[2], bytes[3]);
+	return (*old_connect)(sock, uaddr, sockaddr_len, flags);
 }
 static int init(void)
 {
